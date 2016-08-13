be553e6344b5413c8c8fc6d4af640690cfcddbaf
Akce <>
2016-08-13 12:33:24 +1000

    Fix dumpkb ute, add some labels to output.

--- bin/dumpkb.py
+++ bin/dumpkb.py
@@ -6,17 +6,21 @@ Small script to see how X keycodes & modifiers are all tied together.
 
 Copyright (c) 2016 Akce
 """
-import ctypes
 import pprint
 
 import footwm.kb as kb
-import footwm.xlib as xlib
+import footwm.display as display
 
 if __name__ == '__main__':
-    d = xlib.xlib.XOpenDisplay(None)
+    d = display.Display()
     keyboard = kb.Keyboard(d)
+    print('Keysym groups:')
     pprint.pprint(keyboard.keysymgroups, width=200)
+    print('Modifier codes:')
     pprint.pprint(keyboard.keymodifiercodes, width=200)
+    print('Modifier keysyms:')
     pprint.pprint(keyboard.keymodifiersyms, width=200)
+    print('Keycodes:')
     pprint.pprint(keyboard.keycodes, width=200)
+    print('Modifiers:')
     pprint.pprint(keyboard.modifiers, width=200)
