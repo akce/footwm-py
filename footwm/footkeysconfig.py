6a1437556d5251a90614865358972da92b41d21c
Akce <>
2017-03-26 11:41:19 +1100

    Make running footrun a little easier.
    No longer necessary to always specify the runner address. A sensible
    default is provided.
    
    Example starting an xterm is added to the default footkeys config file.

--- footwm/footkeysconfig.py
+++ footwm/footkeysconfig.py
@@ -1,11 +1,16 @@
 import functools
 
+from .footrun import run
+
+TERMINAL = 'xterm'
+
 # Order windows using most recently used.
 stacking = True
 
 # Require scroll-lock for all our keys and always ignore caps-lock and numlock.
 # This way, we can use the function keys for regular actions (if required by apps) by toggling off scroll-lock.
 capslock = xlib.KeyModifierMask.Lock
+alt = xlib.KeyModifierMask.Mod1
 numlock = xlib.KeyModifierMask.Mod2
 scrolllock = xlib.KeyModifierMask.Mod3
 keyconfig.setmodifiers(requiremods=[scrolllock], ignoremods=[capslock, numlock])
@@ -21,3 +26,6 @@ keyconfig.addkey('F9', functools.partial(client.selectdesktop, index=1))
 keyconfig.addkey('F10', functools.partial(client.selectdesktop, index=2))
 keyconfig.addkey('F11', functools.partial(client.selectdesktop, index=3))
 keyconfig.addkey('F12', functools.partial(client.deletedesktop, index=0))
+
+# ALT + ENTER : Runs an xterm. Requires a running footrun daemon.
+keyconfig.addkey('Return', requiremods=[alt], action=functools.partial(run, TERMINAL))
--- footwm/footrun.py
+++ footwm/footrun.py
@@ -35,27 +35,35 @@ class SelectRunner:
         self._loop.add_client(server, timeout=0)
         self._loop.serve_forever()
 
-def daemonstart(args):
+def daemonstart(sockname):
     """ Start a footrun daemon instance. """
     # Since we're using Unix sockets, we need to remove stale socket files or
     # we'll get Address in use errors when we try to connect.
     try:
-        st = os.stat(args.sockname)
+        st = os.stat(sockname)
     except (FileNotFoundError, OSError) as e:
-        pass
+        # Make sure to create the directory to the socket file.
+        dir_, path = os.path.split(sockname)
+        if dir_:
+            os.makedirs(dir_, exist_ok=True)
     else:
         # Only remove the file if it is a unix domain socket file.
         if stat.S_ISSOCK(st.st_mode):
-            os.unlink(args.sockname)
+            os.unlink(sockname)
         else:
             # File exists but is not a socket, don't remove and halt execution!
-            print('File {} exists and is not a unix socket. Remove or specify alternate filename.'.format(args.sockname), file=sys.stderr)
+            log.error('File %s exists and is not a unix socket. Remove or use alternate filename.', sockname)
             sys.exit(1)
     run = SelectRunner()
-    server = selectloop.StreamServer(address=args.sockname, family=socket.AF_UNIX, newconn=run.addclient)
+    server = selectloop.StreamServer(address=sockname, family=socket.AF_UNIX, newconn=run.addclient)
     run.go(server)
 
-def run(address, cmdline):
+def clidaemonstart(args):
+    daemonstart(sockname=args.sockname)
+
+def run(cmdline, address=None):
+    if address is None:
+        address = getdefaultaddress()
     remote = selectloop.StreamClient(address=address, family=socket.AF_UNIX)
     remote.connect()
     runner = jsonrpc.RemoteObject(['run'], postfunc=remote.post)
@@ -64,17 +72,20 @@ def run(address, cmdline):
 def clirun(args):
     run(address=args.sockname, cmdline=' '.join('"{}"'.format(x) for x in args.args))
 
+def getdefaultaddress():
+    return os.path.join(os.environ['HOME'], '.foot', 'run.sock')
+
 def makeargparser():
     parser = argparse.ArgumentParser()
 
     connparser = argparse.ArgumentParser(add_help=False)
-    connparser.add_argument('--sockname', default='footrun.sock', help='unix socket filename. default: %(default)s')
+    connparser.add_argument('--sockname', default=getdefaultaddress(), help='unix socket filename. default: %(default)s')
 
     commands = nestedarg.NestedSubparser(parser.add_subparsers())
     with commands('daemon', aliases=['d'], parents=[connparser], help='app running daemon commands') as c:
         d = nestedarg.NestedSubparser(c.add_subparsers())
         with d('start', help='start a footrun daemon') as d1:
-            d1.set_defaults(command=daemonstart)
+            d1.set_defaults(command=clidaemonstart)
     with commands('run', aliases=['e', 'r'], parents=[connparser], help='run (execute) a command') as c:
         c.set_defaults(command=clirun)
         # TODO specify command output logging options.
