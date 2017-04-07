6d2ef9ead6341491675f1636511d239cfd8255c7
Akce <>
2017-04-07 16:28:37 +1000

    Add footmenu desktops/windows to default keyconfig.

--- footwm/footkeysconfig.py
+++ footwm/footkeysconfig.py
@@ -16,16 +16,22 @@ scrolllock = xlib.KeyModifierMask.Mod3
 keyconfig.setmodifiers(requiremods=[scrolllock], ignoremods=[capslock, numlock])
 
 # Window select.
-keyconfig.addkey('F5', functools.partial(client.activatewindow, stacking=stacking, index=1))
-keyconfig.addkey('F6', functools.partial(client.activatewindow, stacking=stacking, index=2))
-keyconfig.addkey('F7', functools.partial(client.activatewindow, stacking=stacking, index=3))
-keyconfig.addkey('F8', functools.partial(client.closewindow, stacking=stacking, index=0))
+keyconfig.addkey('F1', functools.partial(client.activatewindow, stacking=stacking, index=1))
+keyconfig.addkey('F2', functools.partial(client.activatewindow, stacking=stacking, index=2))
+keyconfig.addkey('F3', functools.partial(client.activatewindow, stacking=stacking, index=3))
+keyconfig.addkey('F4', functools.partial(client.closewindow, stacking=stacking, index=0))
 
 # Group select.
-keyconfig.addkey('F9', functools.partial(client.selectdesktop, index=1))
-keyconfig.addkey('F10', functools.partial(client.selectdesktop, index=2))
-keyconfig.addkey('F11', functools.partial(client.selectdesktop, index=3))
-keyconfig.addkey('F12', functools.partial(client.deletedesktop, index=0))
+keyconfig.addkey('F5', functools.partial(client.selectdesktop, index=1))
+keyconfig.addkey('F6', functools.partial(client.selectdesktop, index=2))
+keyconfig.addkey('F7', functools.partial(client.selectdesktop, index=3))
+keyconfig.addkey('F8', functools.partial(client.deletedesktop, index=0))
+
+# Menus.
+# Window select menu.
+keyconfig.addkey('F9', functools.partial(run, " ".join([TERMINAL, '-e', 'footmenu', 'w'])))
+# Desktop select menu.
+keyconfig.addkey('F10', functools.partial(run, " ".join([TERMINAL, '-e', 'footmenu', 'd'])))
 
 # ALT + ENTER : Runs an xterm. Requires a running footrun daemon.
 keyconfig.addkey('Return', requiremods=[alt], action=functools.partial(run, TERMINAL))
