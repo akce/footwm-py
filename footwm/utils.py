bd224491719df5a046187d0a18bba78773eab775
Akce <>
2017-05-16 07:42:23 +1000

    Memoise only meaningful values.
    Had a problem where the sizehints for emacs was being memoised while
    it was still None. Not an ideal solution but it works for now.
    
    Seems that emacs does not have all the window members set at
    CreateNotify time (the point at which these are failing).
    
    A future solution might be to instanciate the window at a later
    point. eg, ConfigureRequest/MapRequest etc.

--- footwm/utils.py
+++ footwm/utils.py
@@ -13,7 +13,12 @@ def memoise(func):
         # kwargs is a dict and can't be hashed. Trialling id as a workaround.
         key = (args, id(kwargs))
         if key not in results:
-            results[key] = func(*args, **kwargs)
-            #print('memoise: added result {} for key {}'.format(results[key], key))
-        return results[key]
+            result = func(*args, **kwargs)
+            # Only store non None results. Having a problem where
+            # we're memoising a bit too early still so make sure we
+            # only store worthwhile values.
+            if result is not None:
+                results[key] = result
+            #print('memoise: added result {} for key {}'.format(result, key))
+        return results.get(key, None)
     return memoiser
