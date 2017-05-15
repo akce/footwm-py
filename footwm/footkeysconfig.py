c3de394190dfef8cc74246ceb4cd8dde609a8337
Akce <>
2017-05-15 17:36:58 +1000

    Improve default footkeys configuration.
    Added the app menu.
    Moved potentially problematic desktop/window-kill keys to less
    prominent function keys.

--- footwm/footkeysconfig.py
+++ footwm/footkeysconfig.py
@@ -17,19 +17,25 @@ keyconfig.setmodifiers(requiremods=[scrolllock], ignoremods=[capslock, numlock])
 keyconfig.addkey('F1', action=do(client.activatewindow, stacking=stacking, index=1))
 keyconfig.addkey('F2', action=do(client.activatewindow, stacking=stacking, index=2))
 keyconfig.addkey('F3', action=do(client.activatewindow, stacking=stacking, index=3))
-keyconfig.addkey('F4', action=do(client.closewindow, stacking=stacking, index=0))
+keyconfig.addkey('F4', action=do(client.activatewindow, stacking=stacking, index=4))
 
 # Group select.
 keyconfig.addkey('F5', action=do(client.selectdesktop, index=1))
 keyconfig.addkey('F6', action=do(client.selectdesktop, index=2))
 keyconfig.addkey('F7', action=do(client.selectdesktop, index=3))
-keyconfig.addkey('F8', action=do(client.deletedesktop, index=0))
+keyconfig.addkey('F8', action=do(client.selectdesktop, index=4))
 
 # Menus.
 # Window select menu.
 keyconfig.addkey('F9', action=do(run, TERMINAL + ' -e footmenu w'))
 # Desktop select menu.
 keyconfig.addkey('F10', action=do(run, TERMINAL + ' -e footmenu d'))
+
+keyconfig.addkey('F11', action=do(client.closewindow, stacking=stacking, index=0))
+keyconfig.addkey('F12', action=do(client.deletedesktop, index=0))
+
+# App menu. I bind F20 to CAPSLOCK.
+keyconfig.addkey('F20', action=do(run, TERMINAL + ' -e appmenu'))
 
 # ALT + ENTER : Runs an xterm. Requires a running footrun daemon.
 keyconfig.addkey('Return', requiremods=[alt], action=do(run, TERMINAL))
